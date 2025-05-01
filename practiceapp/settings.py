009e3585992feb651c9ad8b49dd3397b1c8d6212
Vaishnavi Reddy <vaishnavireddy1120@gmail.com>
2025-05-01 15:54:38 +0530

    Update settings.py

--- practiceapp/settings.py
+++ practiceapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '5%%@oahp6+8@gyv-a6&zc6+*u42lu!lx(ctnq2ban0ef3t_4)z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [''crime-analytics-web-app-1.onrender.com'','127.0.0.1']
+ALLOWED_HOSTS = ['crime-analytics-web-app-1.onrender.com','127.0.0.1']
 
 
 # Application definition
